2d7a8bb45d095978b2be665215b2da29ba01ba4b
manuel <manuelmat41@gmail.com>
2022-11-09 10:55:37 +0100

    a

---
+++ Tema6/bol6_ejer4/bol6_ejer4/ImageInfo.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace bol6_ejer4
+{
+    public class ImageInfo
+    {
+        public ImageInfo(String path)
+        {
+            this.Path = path;
+        }
+
+        public bool IsBMP()
+        {
+            if (!File.Exists(Path))
+            {
+                return false;
+            }
+            try
+            {
+                using (BinaryReader br = new BinaryReader(new FileStream(Path, FileMode.Open)))
+                {
+                    if (new string(br.ReadChars(2)) != "BM")
+                    {
+                        return false;
+                    }
+                    if (new FileInfo(Path).Length != br.ReadInt32())
+                    {
+                        return false;
+                    }
+                    br.BaseStream.Seek(0x0E, SeekOrigin.Begin);
+                    if (br.ReadInt32() != 40)
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            return true;
+        }
+
+        public InfoBMP InfoBMP()
+        { 
+            if (!IsBMP())
+            {
+                throw new ArgumentException("Is not a BMP file");
+            }
+            InfoBMP infoBMP;
+            using (BinaryReader br = new BinaryReader(new FileStream(Path, FileMode.Open)))
+            {
+                br.BaseStream.Seek(0x12, SeekOrigin.Begin);
+                infoBMP.pixelWidth = br.ReadInt32();
+                infoBMP.pixelHeight = br.ReadInt32();
+                br.BaseStream.Seek(0x1E, SeekOrigin.Begin);
+                infoBMP.compressed = br.ReadInt32() > 0;
+                br.BaseStream.Seek(0x1C, SeekOrigin.Begin);
+                infoBMP.bitsPerPixel = br.ReadInt16();
+            }
+            return infoBMP;
+        }
+
+        public string Path;
+    }
+
+    public struct InfoBMP
+    {
+        public int pixelWidth;
+        public int pixelHeight;
+        public bool compressed;
+        public short bitsPerPixel;
+
+    }
+}
--- Tema6/bol6_ejer4/bol6_ejer4/Program.cs
+++ Tema6/bol6_ejer4/bol6_ejer4/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace bol6_ejer4
+﻿
+namespace bol6_ejer4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ImageInfo imageInfo = new ImageInfo("C:\\Users\\Manu\\Documents\\Sin título.bmp");
+            InfoBMP info = imageInfo.InfoBMP();
+            Console.WriteLine($"Size: {info.pixelWidth}x{info.pixelHeight}\nCompressed: {info.compressed}\nBits per pixel: {info.bitsPerPixel}");
         }
     }
 }
